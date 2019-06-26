263009b7a499fcb7ec4fab242c85c45f088ad91a
cruel-32 <cruel32@nate.com>
2019-06-26 21:53:19 +0900

    first commit ^_^

---
+++ practice/for.py
@@ -0,0 +1,19 @@
+forList = [0,1,2,3,4,5]
+
+for c in forList:
+    print(c)
+else:
+    print('for success')
+
+
+dictionaryTest = {
+    'a':1,
+    'b':2,
+    'c':3,
+    'd':4,
+}
+
+for c in dictionaryTest:
+    print(dictionaryTest[c])
+else:
+    print('for success')
