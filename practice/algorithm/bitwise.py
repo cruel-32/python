0f1b26d8e805361fb2ab4f74b0e06d767a5ec15c
cruel32 <cshee8508@gmail.com>
2019-12-15 17:25:33 +0900

    앞자리 공백을 0으로 넣기

--- practice/algorithm/bitwise.py
+++ practice/algorithm/bitwise.py
@@ -13,9 +13,10 @@ def getBitwise(n,arr1,arr2):
                 txt='#'+txt
             elif(nmg==0):
                 txt=' '+txt
+        if(len(txt) < 6):
+            txt = txt.rjust(6, ' ')
+            print('fill')
         arr3.append(txt)
     return arr3
-
 print( getBitwise(5,[9,20,28,18,11],[30,1,21,17,28]) )
-
-
+print( getBitwise(6,[46,33,33,22,31,1],[27,56,19,14,14,1]) )
--- practice/set.py
+++ practice/set.py
@@ -1,7 +1,10 @@
-s1 = set([5,4,12,10,14,1,2,19])
+list1 = [5, 4, 12, 10, 14, 1, 2, 19]
+list2 = [4, 8, 2, 10, 5, 6, 7, 8, 13]
+
+s1 = set(list1)
 print(s1)
 
-s2 = set([4,8,2,10,5,6,7,8,13])
+s2 = set(list2)
 print(s2)
 
 print('합집합 => ', s1 & s2)
@@ -18,5 +21,6 @@ print('합집합 => ', s1.intersection(s2))
 print('차집합 => ', s1 - s2)
 print('차집합 => ', s1.difference(s2))
 
-
 s1.remove(13)
+
+
