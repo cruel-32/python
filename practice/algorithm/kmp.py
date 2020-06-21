2d2aa1a8d9898e3f9bf7b365d3f4f6650bf7ffee
cruel32 <cshee8508@gmail.com>
2020-06-21 16:42:18 +0900

    kmp 알고리즘

--- practice/algorithm/kmp.py
+++ practice/algorithm/kmp.py
@@ -1,38 +1,51 @@
 import math
 
 def getPi(str):
-    pi = []
-    e = 0
+    pi = [ 0 for x in range(len(str)) ]
+    j = 0
 
     for i in range(len(str)):
-        if i == 0 :
-            pi.append(i)
-        else :
-            s = 0
-            e = i+1
-            words = str[0:e]
-            print('words : ', words)
+        if i == 0:
+            continue
 
-            # pi.append(s)
+        while(j > 0 and str[i] is not str[j]):
+            j = pi[j-1]
+
+        if str[i] == str[j]:
+            j += 1
+            pi[i] = j
+
     return pi
-
 # 'abcdabcdabca', 
 
-print( getPi('abcdabca') )
+def kmp(pa, str):
+    ans = []
+    pi = getPi(str)
+    j = 0
 
+    for i in range(len(pa)):
+        while(j > 0 and pa[i] is not str[j]):
+            j = pi[j-1]
+
+        if pa[i] == str[j]:
+            if j == len(str)-1:
+                ans.append( i - len(str) + 1 )
+                j = pi[j]
+            else:
+                j+=1
+
+    return ans
+                
+print('kmp :: ', kmp('abcdabcdabcaabcdabca', 'abcdabca'))
 
 # abcdabcdabca abcdabca
 
 # 접두사와 접미사가 일치하는 숫자 구하기
-# 0 a         0
-# 1 ab        0  0 1 1 짝
-# 2 abc       0  0 1 2 홀
-# 3 abcd      0  0 2 2 짝
-# 4 abcda     1  0 2 3 홀
-# 5 abcdab    2  0 3 3 짝
-# 6 abcdabc   3  0 3 4 홀
-# 7 abcdabca  0  0 4 4 짝
-
-
-
-
+# 0 a         0 //한글자일땐 패스
+# 1 ab        0
+# 2 abc       0
+# 3 abcd      0
+# 4 abcda     1
+# 5 abcdab    2
+# 6 abcdabc   3
+# 7 abcdabca  1
